998a8a03d99c03e7f32cbd5948bec1281dcc3e3f
Savitha1125 <savithasengottaiyan7@gmail.com>
2025-07-18 15:51:21 +0530

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,6 @@
+        function display(){
+            document.getElementById('out').innerHTML= 'Welcome';
+            let a=document.getElementById('name').value 
+            alert("hi");
+            console.log(a)
+            }
